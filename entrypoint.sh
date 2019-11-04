1ae61323d855ce4a6b342544d2238a4db3d426f1
Max Hacking <max@hacking.co.uk>
2019-11-04 15:04:11 +0000

    Output "branding" and list of processed files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "${GITHUB_WORKSPACE}" ]]; then
-    echo "Must set GITHUB_WORKSPACE in env"
+function die()
+{
+    echo "::error::$1"
+    echo "------------------------------------------------------------------------------------------------------------------------"
     exit 1
-fi
+}
 
-cd "${GITHUB_WORKSPACE}" || exit 2
+cat << END
+------------------------------------------------------------------------------------------------------------------------
+                                         _          _ _      _               _    
+                                        | |        | | |    | |             | |   
+                                     ___| |__   ___| | | ___| |__   ___  ___| | __
+                                    / __| '_ \ / _ \ | |/ __| '_ \ / _ \/ __| |/ /
+                                    \__ \ | | |  __/ | | (__| | | |  __/ (__|   < 
+                                    |___/_| |_|\___|_|_|\___|_| |_|\___|\___|_|\_\\
+ 
+                           https://github.com/hacking-actions/shellcheck (c) 2019 Max Hacking 
+------------------------------------------------------------------------------------------------------------------------
+END
 
-ls -la
-echo -e "\n"
+# Check for a GITHUB_WORKSPACE env variable
+[[ -z "${GITHUB_WORKSPACE}" ]] && die "Must set GITHUB_WORKSPACE in env"
+cd "${GITHUB_WORKSPACE}" || die "GITHUB_WORKSPACE does not exist"
 
 exitval=0
 
-for f in $(find . -type f ! -iname ".*" ! -path "./.*" -printf '%P\n')
+while IFS= read -r -d '' f
 do
     if [[ "$(mimetype -b "${f}")" == "application/x-shellscript" ]]; then
+        echo "Checking ${f}"
         shellcheck -s bash "${f}" || exitval=1
     fi
-done
+done <   <(find . -type f ! -iname ".*" ! -path "./.*" -print0)
 
+echo "------------------------------------------------------------------------------------------------------------------------"
 exit ${exitval}
